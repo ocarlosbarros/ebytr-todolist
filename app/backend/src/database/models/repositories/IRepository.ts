d1fcb78c1774219ada1bdc01e81ada663aa82e94
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-02 19:46:55 -0300

    feat:creates IRepository interface

--- app/backend/src/database/models/repositories/IRepository.ts
+++ app/backend/src/database/models/repositories/IRepository.ts
@@ -1,9 +1,11 @@
 interface IRepository<T> {
     create(entity: T): Promise<T>;
     findAll(): Promise<Array<T>>;
-    findById(id: number):Promise<T|void>
+    findById(id: number):Promise<T|null>
     update(entity: T): Promise<T>;
     delete(id: number): Promise<void>;
 }
+    
+       
 
 export default IRepository;
