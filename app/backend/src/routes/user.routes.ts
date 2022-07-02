37ed7a98ca1d42278620b5e16a0ef8188038b770
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-02 19:47:22 -0300

    feat:creates userRouter

--- app/backend/src/routes/user.routes.ts
+++ app/backend/src/routes/user.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import UserController from '../controllers/user.controller';
+import UserService from '../services/user.service';
+import UserRepository from '../database/models/repositories/user.repository';
 
 const userRouter = Router();
 
-const userController = new UserController();
+const userRepository = new UserRepository()
+const userService = new UserService(userRepository);
+const userController = new UserController(userService);
 
-userRouter.get('/users', userController.findAll);
+userRouter.get('/', userController.findAll);
 
 export default userRouter;
